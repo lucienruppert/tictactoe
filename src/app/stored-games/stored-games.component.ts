36feb7225ac649da6bae44eab0b2537610eee10c
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-03-10 17:53:53 +0100

    add stored-games component

---
+++ src/app/stored-games/stored-games.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stored-games',
+  imports: [],
+  templateUrl: './stored-games.component.html',
+  styleUrl: './stored-games.component.css'
+})
+export class StoredGamesComponent {
+
+}
