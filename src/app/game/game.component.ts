8afde88e28008c0073307e6c9be4d2dfb92ec244
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-03-10 18:20:24 +0100

    create default game board

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   standalone: true,
 })
-export class GameComponent {}
+export class GameComponent implements OnInit {
+  tableSize: number = 3;
+  gameState: string;
+  isSaved: boolean = false;
+
+  constructor() {
+    this.gameState = '0'.repeat(Math.pow(this.tableSize, 2));
+  }
+
+  ngOnInit() {
+    console.log(this.gameState);
+  }
+
+  getGameStateForBoard(): string[][] {
+    const matrix: string[][] = [];
+    for (let i = 0; i < this.tableSize; i++) {
+      matrix.push(
+        this.gameState
+          .slice(i * this.tableSize, (i + 1) * this.tableSize)
+          .split('')
+      );
+    }
+    return matrix;
+  }
+}
